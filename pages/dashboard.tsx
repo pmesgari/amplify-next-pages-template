0e174efa60faa3328a8fb098a768b6979c9bf7d3
Payam <payam.mesgari@gmail.com>
2024-11-10 16:04:30 +0100

    program module list

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,15 +1,19 @@
 import { useEffect, useState } from "react";
 import { useAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-
+import type { Schema } from "../amplify/data/resource";
+import { generateClient } from "aws-amplify/data";
 import { Authenticator } from '@aws-amplify/ui-react'
 import { getCurrentUser } from 'aws-amplify/auth';
 import '@aws-amplify/ui-react/styles.css';
 import { fetchAuthSession } from 'aws-amplify/auth';
 
+const client = generateClient<Schema>();
+
 function Programs() {
   const { signOut } = useAuthenticator();
   const [currentUser, setCurrentUser] = useState('');
+  const [programModules, setProgramModules] = useState<Array<Schema["ProgramModule"]["type"]>>([]);
 
   useEffect(() => {
 
@@ -25,6 +29,11 @@ function Programs() {
         console.error("Error fetching current user:", error);
       }
     };
+    
+    // const res = client.models.ProgramModule.observeQuery({authMode: 'userPool'})
+    client.models.ProgramModule.observeQuery({authMode: 'userPool'}).subscribe({
+      next: (data) => setProgramModules([...data.items]),
+    });
 
     fetchUser();
     
@@ -40,6 +49,18 @@ function Programs() {
     <section className='section'>
       <button onClick={signOut}>Sign Out</button>
     </section>
+    <section className="section">
+        {programModules.map((module) => (
+          <div key={module.id}>
+            <div>
+              <p>{module.id}</p>
+              <p>{module.section}</p>
+              <p>{module.module}</p>
+            </div>
+            <br />
+          </div>
+        ))}
+      </section>
   </main>
   )
 }
